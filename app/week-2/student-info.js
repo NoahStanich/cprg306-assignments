99da5cefb2d4e27d8f9046c7674d667a94b4c1e9
NoahStanich <noah.stanich@edu.sait.ca>
2025-09-11 22:36:22 -0600

    Completed week 2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,8 @@
+export default function StudentInfo(){
+    return(
+        <div>
+            <p>Noah Stanich</p>
+            <a href="https://github.com/NoahStanich/cprg306-assignments">My github</a>
+        </div>
+    );
+}
